f13fb825eed4e691f07accad334fb17d6be45636
diasjulia <jedl@cin.ufpe.br>
2022-12-28 20:39:53 -0300

    Feat: Basic Home Sections

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,21 +1,62 @@
 import React from 'react'
 import {Navbar} from '../../components';
 
-import { Container, Section } from './styles';
-import { RoundLogo } from '../../assets';
+import { Container, FirstSection, ThirdSection, RoundLogoImg, RobotImg, RobotImg2, Row, FourthSection, Div, RowItem } from './styles';
+import { RoundLogo, RobotPhotos, Trophy, MagGlasses, Chip, Trophy2, Upload, People } from '../../assets';
 
 const HomePage: React.FC = () => (
   <div>
   <Navbar/>
   <Container>
-    <Section>
-      <img src={RoundLogo} alt="" />
+    <FirstSection>
+      <RoundLogoImg src={ RoundLogo } alt="" />
       <div>
         <h1>Pessoas que desenvolvem...</h1>
         <p>A partir da realização de parcerias, somos uma ferramenta de formação em robótica que envolve uma pluralidade de áreas do conhecimento, principalmente engenharia de hardware e software, gerando oportunidades de trabalho e pesquisa nessa área.</p>
       </div>
-      
-    </Section>
+      <RobotImg src={ RobotPhotos } alt="" />
+    </FirstSection>
+    <ThirdSection>
+      <h2>O que fazemos</h2>
+      <Row>
+        <RowItem>
+          <img src={ Trophy } alt="Desenho de Troféu" />
+          <h3>Competições</h3>
+          <p>Nós participamos de diversas competições, como a RoboCup, a IRONCup e a LARC.</p>
+        </RowItem>
+        <RowItem>
+          <img src={ MagGlasses } alt="Desenho de Lupa" />
+          <h3>Pesquisa</h3>
+          <p>Professores e estudantes pesquisando dentro dos problemas da robótica.</p>
+        </RowItem>
+        <RowItem>
+          <img src={ Chip } alt="Desenho de Processador" />
+          <h3>Desenvolvimento</h3>
+          <p>Nós projetamos e desenvolvemos a eletrônica, a mecânica e a inteligência dos robôs.</p>
+        </RowItem>
+      </Row>
+    </ThirdSection>
+    <FourthSection>
+      <h2>Quem somos nós</h2>
+      <Row>
+        <p>RobôCIn é um grupo de pesquisa e desenvolvimento do Centro de Informática da UFPE, que utiliza a robótica para desenvolver soluções. O grupo foi criado em 2015 por 12 estudantes de Engenharia da Computação com ajuda de dois professores. Edna Barros e Hansenclever Bassani. Desenvolver soluções utilizando Inteligência Artificial, Visão Computacional, Mecânica e Eletrônica aplicados na robótica.</p>
+        <RobotImg2 src={ RobotPhotos } alt="" />
+      </Row>
+      <Row>
+        <div>
+          <img src={ Trophy2 } alt="Desenho de troféu" />
+          <h4>Premiações</h4>
+        </div>
+        <div>
+          <img src={ Upload } alt="Desenho de Upload" />
+          <h4>Artigos publicados</h4>
+        </div>
+        <div>
+          <img src={ People } alt="Desenho de Upload" />
+          <h4>Colaboradores</h4>
+        </div>
+      </Row>
+    </FourthSection>
   </Container>
   </div>
 )
--- src/views/Home/styles.ts
+++ src/views/Home/styles.ts
@@ -1,9 +1,119 @@
 import styled from 'styled-components';
+import { Background1 } from '../../assets';
+
+type DivProps = {
+    width?: string;
+}
 
 export const Container = styled.div`
     margin-top: 85px;`
 ;
 
-export const Section = styled.section`
+export const FirstSection = styled.section`
+    background-color: #EEEEEE;
+    min-height: 364px;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.2), 0px 8px 16px rgba(0, 0, 0, 0.08);
     display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 15px 2rem;
+
+    div{
+        max-width: 512px;
+    }
+
+    h1{
+        font-family: 'Manrope';
+        font-weight: 700;
+        font-size: 30px;
+        line-height: 41px;
+    }
+
+    p{
+        font-family: 'Manrope';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 20px;
+        line-height: 27px;
+        text-align: justify;
+        color: #606060;
+    }
 `;
+
+export const RobotImg = styled.img`
+    margin-right: calc(-24px - 2rem);
+`
+
+export const RoundLogoImg = styled.img``
+
+export const ThirdSection = styled.section`
+    text-align: center;
+
+    background: url(${Background1}) center no-repeat;
+    background-size: auto 110%;
+    box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.2), 0px 8px 16px rgba(0, 0, 0, 0.08);
+
+    padding: 26px 4rem;
+
+    h2{
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 30px;
+        line-height: 41px;
+        color: #1A3C21;
+    }
+
+    h3{
+        text-transform: uppercase;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 27px;
+        color: #1A3C21;
+    }
+`
+
+export const RowItem = styled.div`
+        max-width: 360px;
+        min-width: 300px;
+`
+
+export const Row = styled.div`
+    display:flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+`
+
+export const Div = styled.div<DivProps>`
+    width: ${(props)=> props.width || 'auto'};
+`
+
+export const FourthSection = styled.section`
+    text-align: center;
+    padding: 56px 4rem;
+
+    h2{
+        font-weight: 700;
+        font-size: 30px;
+        line-height: 41px;
+        text-align: center;
+        text-transform: uppercase;
+
+        color: #1A3C21;
+    }
+
+    p{
+        max-width: 600px;
+        font-weight: 500;
+        font-size: 24px;
+        line-height: 33px;
+        text-align: justify;
+
+        color: #606060;
+    }
+`
+
+export const RobotImg2 = styled.img`
+    margin-left: 2rem;
+    width: 45%;
+`
