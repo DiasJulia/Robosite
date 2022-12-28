3fac97a65c8a7c15471e30c8b3e27a4ca6323e4d
diasjulia <jedl@cin.ufpe.br>
2022-12-28 20:38:51 -0300

    Config: Adding assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,13 @@
 import Logo from './logo.svg';
 import RoundLogo from './round-logo.svg';
+import RobotPhotos from './robot-photos.svg';
+import Trophy from './trophy.svg';
+import MagGlasses from './magglasses.svg';
+import Chip from './chip.svg';
+import People from './people.svg';
+import Upload from './upload.svg';
+import Trophy2 from './trophy-2.svg';
 
-export { Logo, RoundLogo };
+import Background1 from './background-1.svg';
+
+export { Logo, RoundLogo, RobotPhotos, Trophy, MagGlasses, Chip, People, Upload, Trophy2, Background1 };
